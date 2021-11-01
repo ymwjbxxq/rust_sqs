299fdf12b1884e39b04980bc931cb26fe517baf2
Daniele Frasca <daniele.frasca@prosiebensat1digital.de>
2021-11-01 19:46:25 +0100

    replace Some with if let

--- src/library/lambda.rs
+++ src/library/lambda.rs
@@ -1,16 +1,15 @@
 pub mod handler {
-  use uuid::Uuid;
-  use crate::queries::get_product_by_id_query::GetById;
-  use crate::queries::get_product_by_id_query::GetByIdQuery;
-  use crate::queries::add_product_query::AddProduct;
-  use crate::queries::add_product_query::AddQuery;
   use crate::dtos::request::Request;
   use crate::models::product::Product;
+  use crate::queries::add_product_query::AddProduct;
+  use crate::queries::add_product_query::AddQuery;
+  use crate::queries::get_product_by_id_query::GetById;
+  use crate::queries::get_product_by_id_query::GetByIdQuery;
   use crate::AWSClient;
   use aws_lambda_events::event::sqs::SqsEvent;
   use futures::stream::{self, StreamExt};
   use lambda_runtime::{Context, Error};
-
+  use uuid::Uuid;
 
   pub async fn execute(client: &AWSClient, event: SqsEvent, _ctx: Context) -> Result<Option<Product>, Error> {
     log::info!("EVENT {:?}", event);
@@ -18,49 +17,55 @@ pub mod handler {
 
     log::info!("records {:?}", records);
     // processing one element at a time
-    while let Some(record) = records.next().await { 
-      match record.body {
-        Some(body) => {
-          let request: Request = serde_json::from_str(&body).unwrap();
-          match request.pk {
-            Some(pk) => {
-              let product = GetById::new()
-                            .await
-                            .execute(&client.dynamo_db_client, &pk)
-                            .await?;
-
-              // send to sqs
-              let sqs_url = std::env::var("OUTPUT_SQS").expect("OUTPUT_SQS must be set");
-              let msg_body = serde_json::to_string(&product).unwrap();
-              let result = client
-                .sqs_client
-                .send_message()
-                .queue_url(sqs_url)
-                .message_body(msg_body)
-                .send()
-                .await;
-              log::info!("SQS sent {:?}", result);
-            }
-            None => {
-              let product = Product{
-                pk:  Uuid::new_v4().to_string(),
-                name: request.name.unwrap(),
-                price: request.price.unwrap(),
-              };
-              AddProduct::new()
-                .await
-                .execute(&client.dynamo_db_client, &product)
-                .await?;
-            }
-          }
+    while let Some(record) = records.next().await {
+      if let Some(body) = record.body {
+        let request: Request = serde_json::from_str(&body).unwrap();
+        if let Some(pk) = request.pk {
+          send_to_sqs(&client, &pk).await?;
+        } else {
+          add(&client, request).await?;
         }
-        None => {
-          log::error!("empty body");
-        }
+      } else {
+        log::error!("empty body");
       }
     }
 
     Ok(None)
   }
 
+  async fn send_to_sqs(client: &AWSClient, pk: &str) -> Result<(), Error> {
+    let product = GetById::new()
+      .await
+      .execute(&client.dynamo_db_client, &pk)
+      .await?;
+
+    // send to sqs
+    let sqs_url = std::env::var("OUTPUT_SQS").expect("OUTPUT_SQS must be set");
+    let msg_body = serde_json::to_string(&product).unwrap();
+    let result = client
+      .sqs_client
+      .send_message()
+      .queue_url(sqs_url)
+      .message_body(msg_body)
+      .send()
+      .await?;
+    log::info!("SQS sent {:?}", result);
+
+    Ok(())
+  }
+
+  async fn add(client: &AWSClient, request: Request) -> Result<(), Error> {
+    let product = Product {
+      pk: Uuid::new_v4().to_string(),
+      name: request.name.unwrap(),
+      price: request.price.unwrap(),
+    };
+    AddProduct::new()
+      .await
+      .execute(&client.dynamo_db_client, &product)
+      .await?;
+
+    Ok(())
+  }
 }
+
