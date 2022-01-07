0f0aa6f7817927966df313338ed7acaa0d0e050c
Daniele <>
2022-01-07 14:44:44 +0100

    remove the old script

--- deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cross build --release --target x86_64-unknown-linux-musl && \
-zip -j rust.zip ./target/x86_64-unknown-linux-musl/release/bootstrap && \
-sam deploy --guided --profile test
